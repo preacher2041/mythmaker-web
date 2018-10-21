aa127e40fe092ad2c379d5fe41e707863d4d6082
Lee Hitchcock <>
2018-10-21 10:17:44 +0100

    LH: Added new action for logOut

--- src/js/actions/index.js
+++ src/js/actions/index.js
@@ -1,4 +1,9 @@
-export const signedIn = () => ({
+export const signIn = () => ({
 	type: 'USER_SIGNED_IN',
 	userSignedIn: true
+});
+
+export const signOut = () => ({
+	type: 'USER_SIGNED_OUT',
+	userSignedOut: false
 });
